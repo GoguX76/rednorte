5540158d7b134d3374da3156135eecd05a5e4e0c
GoguX76 <se.godoyr@duocuc.cl>
2026-05-19 01:26:17 -0400

    feat(bff): implementacion de seguridad JWT y sincronizacion de servicios

--- bff/src/routes/waitlist_routes.ts
+++ bff/src/routes/waitlist_routes.ts
@@ -1,5 +1,6 @@
-// Importacion de Hono para manejar peticiones HTTP y de las funciones del controlador de waitlist
+// Importacion de Hono para manejar peticiones HTTP, seguridad con JWT y usar las funciones del controlador de waitlist
 import { Hono } from "hono";
+import { jwt } from "hono/jwt";
 import {
   addPatientHandler,
   getQueueHandler,
@@ -7,6 +8,11 @@ import {
 } from "../controllers/waitlist_controller";
 
 const waitlistRouter = new Hono();
+// Extraemos la clave y le decimos a TypeScript que es un string
+const secretKey = (Bun.env.JWT_SECRET || "clave_secreta_desarrollo") as string;
+
+// Todo lo que pase por aquí debe tener un token válido
+waitlistRouter.use("/*", jwt({ secret: secretKey } as any));
 
 // Definimos las rutas para las diferentes funciones
 waitlistRouter.post("/", addPatientHandler);
--- bff/src/services/auth_client.ts
+++ bff/src/services/auth_client.ts
@@ -1,16 +1,16 @@
 // Variable que almacena la URL con la que se conecta con el BFF
-const AUTH_API_URL = "http://ms-users:3000/api/auth/users";
+const AUTH_API_URL = "http://ms-users:3001/users/login";
 
 // Objeto constante que contiene los métodos para la autenticación en el login
 export const AuthClient = {
-  login: async (username: string, password: string) => {
+  login: async (email: string, password: string) => {
     //Método que manda las credenciales a la URL en formato JSON
-    const response = await fetch("http://localhost:8081/api/auth/users", {
+    const response = await fetch(AUTH_API_URL, {
       method: "POST", // Envía las credenciales al microservicio de ms-users
       headers: {
         "Content-Type": "application/json", // Sube las credenciales en formato JSON
       },
-      body: JSON.stringify({ username, password }), // Manda los datos username y password en formato JSON gracias a stringify
+      body: JSON.stringify({ email, password }), // Manda los datos email y password en formato JSON gracias a stringify
     });
 
     // Lanza un error si las credenciales no son válidas o si hubo un error en el servidor
